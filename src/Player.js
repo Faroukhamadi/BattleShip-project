a398ed574b268d0598e283bff62e28e415bb17a6
Farouk Hamadi <faroukhamadi1@outlook.com>
2022-02-07 00:52:11 +0100

    Finished Styling page

--- src/Player.js
+++ src/Player.js
@@ -11,6 +11,9 @@ class Player {
     }
   }
 
+  /**
+   * Makes a random move for the computer player
+   */
   computerMove() {
     if (this.name === 'computer') {
       const gameBoard = new GameBoard();
@@ -18,7 +21,7 @@ class Player {
       do {
         row = Math.floor(Math.random() * 11);
         col = Math.floor(Math.random() * 11);
-      } while (this.isLegalMove(row, col, gameBoard) === false);
+      } while (this.#isLegalMove(row, col, gameBoard) === false);
       gameBoard.receiveAttack(row, col);
     }
   }
@@ -30,7 +33,7 @@ class Player {
    * @param {Object} gameBoard Contains an instance of the GameBoard class
    * @returns True if the move is legal and False otherwise
    */
-  isLegalMove(row, col, gameBoard) {
+  #isLegalMove(row, col, gameBoard) {
     if (this.name === 'computer') {
       // 6 means missed and 7 means destroyed, not a valid move
       if (gameBoard[row][col] >= 6) return false;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import Ship from './Ship';
 import GameBoard from './GameBoard';
+import Player from './Player';
